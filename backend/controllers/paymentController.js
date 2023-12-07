385b44055d0b8a3a8bb134217a1c3d64987929c5
JarmanjitSingh <jarmanjits176@gmail.com>
2023-12-07 23:14:22 +0530

    cancel subscription route done

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -69,3 +69,38 @@ export const getRazorPayKey = catchAsyncErrors(async (req, res, next) => {
     key: process.env.RAZORPAY_API_KEY,
   });
 });
+
+export const cancelSubscription = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  const subscriptionId = user.subscription.id;
+
+  let refund = false;
+
+  await instance.subscriptions.cancel(subscriptionId);
+
+  const payment = await Payment.findOne({
+    razorpay_subscription_id: subscriptionId,
+  });
+
+  const gap = Date.now() - payment.createdAt;
+  const refundTime = process.env.REFUND_DAYS * 24 * 60 * 1000;
+
+  if (refundTime > gap) {
+    await instance.payments.refund(payment.razorpay_payment_id);
+    refund = true
+  }
+
+  await payment.deleteOne();
+  user.subscription.id = undefined;
+  user.subscription.status = undefined;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: refund
+      ? "Subscription cancelled, you will recieve full refund within 7 days"
+      : "Subscription cancelled, no refund initiated as subscription was cancelled after 7 days",
+  });
+});
--- backend/routes/paymentsRoutes.js
+++ backend/routes/paymentsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { buySubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
+import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
@@ -12,6 +12,9 @@ router.route("/paymentverification").post(isAuthenticated, paymentVerification)
 
 //getting the razorpaykey 
 router.route("/razorpaykey").get(getRazorPayKey)
+
+//cancel subscription 
+router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription)
 
 
 
