a6cf288465b94e738ab7312b9f756636d4ca3e59
JarmanjitSingh <jarmanjits176@gmail.com>
2023-12-21 22:12:31 +0530

    adding vercel.json file for solve the refresh problem

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
   const {isAuthenticated, user, message, error} = useSelector(state=> state.user)
   const dispatch = useDispatch();
   const location = useLocation();
-  console.log(location) 
 
   if(location.pathname == "/"){
     document.title = "Code Blu";
